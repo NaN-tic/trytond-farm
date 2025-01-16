9a0cddc9d1415b3965d34d8291a62c28e8bca170
Raimon Esteve <raimonesteve@gmail.com>
2025-01-16 12:43:28 +0100

    Add stock_warehouse_search depends

--- stock.py
+++ stock.py
@@ -187,32 +187,6 @@ class Location(metaclass=PoolMeta):
         return False
 
     @classmethod
-    def __setup__(cls):
-        super().__setup__()
-        cls.warehouse.searcher = 'search_warehouse'
-
-    @classmethod
-    def search_warehouse(cls, name, clause):
-        warehouse_child_locations = cls.search([
-            ('parent.type', '=', 'warehouse'),
-            ('type', '=', 'storage'),
-            ('parent', clause[1], clause[2]),
-            ])
-        found_warehouse_ids = []
-        storage_location_ids = []
-        for location in warehouse_child_locations:
-            storage_location_ids.append(location.id)
-            found_warehouse_ids.append(location.parent.id)
-        warehouse_location_ids = []
-        for location in cls.search([
-                ('parent', 'child_of', storage_location_ids),
-                ]):
-            if (location.warehouse and location.warehouse.id in
-                    found_warehouse_ids):
-                warehouse_location_ids.append(location.id)
-        return [('id', 'in', warehouse_location_ids)]
-
-    @classmethod
     def get_current_lot(cls, locations, name):
         '''
         It suposes that a silo is never filled by same lot two times.
